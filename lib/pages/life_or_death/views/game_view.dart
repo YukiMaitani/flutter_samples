f03db02843c665ad800a4b769214bdd10160a5ff
YukiMaitani <fightzoi1997@gmail.com>
2023-10-28 09:44:54 +0900

    生か死のビューを調整

--- lib/pages/life_or_death/views/game_view.dart
+++ lib/pages/life_or_death/views/game_view.dart
@@ -19,32 +19,37 @@ class _GameViewState extends State<GameView> {
         title: const Text('Life or Death'),
         shadowColor: Colors.transparent,
       ),
-      body: _buildBody(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: _buildBody(),
+      ),
     );
   }
 
   Widget _buildBody() {
-    return Builder(builder: (context) {
-      final gameBloc = BlocProvider.of<GameBloc>(context);
-      final isCorrect = gameBloc.state.isCorrect;
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildScore(gameBloc),
-          _buildInstructionText(isCorrect),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              if (isCorrect == null) ...[
-                Expanded(child: _buildButton(0, gameBloc)),
-                Expanded(child: _buildButton(1, gameBloc))
-              ] else
-                _buildRestartButton(gameBloc),
-            ],
-          ),
-        ],
-      );
-    });
+    final gameBloc = BlocProvider.of<GameBloc>(context);
+    final isCorrect = gameBloc.state.isCorrect;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20),
+        Align(alignment: Alignment.topRight, child: _buildScore(gameBloc)),
+        const SizedBox(height: 100),
+        _buildInstructionText(isCorrect),
+        const Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (isCorrect == null) ...[
+              Expanded(child: _buildButton(0, gameBloc)),
+              Expanded(child: _buildButton(1, gameBloc))
+            ] else
+              _buildRestartButton(gameBloc),
+          ],
+        ),
+        const Spacer(),
+      ],
+    );
   }
 
   Widget _buildInstructionText(bool? isCorrect) {
@@ -86,19 +91,24 @@ class _GameViewState extends State<GameView> {
         gameBloc.add(GameStartEvent());
         setState(() {});
       },
-      child: const Text('Restart'),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      ),
+      child: const Text('Restart', style: TextStyle(fontSize: 28)),
     );
   }
 
   Widget _buildScore(GameBloc gameBloc) {
     final lifeCount = gameBloc.state.lifeCount;
     final dieCount = gameBloc.state.dieCount;
+    const textStyle = TextStyle(fontSize: 20);
     return Column(
       children: [
-        Text('Life: $lifeCount'),
-        Text('Die: $dieCount'),
+        Text('Life: $lifeCount', style: textStyle),
+        Text('Die: $dieCount', style: textStyle),
         Text(
-            'SurvivalRate: ${dieCount + lifeCount > 0 ? '${(lifeCount / (dieCount + lifeCount) * 100).toStringAsFixed(1)}%' : 'NoRecord'}'),
+            'SurvivalRate: ${dieCount + lifeCount > 0 ? '${(lifeCount / (dieCount + lifeCount) * 100).toStringAsFixed(1)}%' : 'NoRecord'}',
+            style: textStyle),
       ],
     );
   }
